e1c73ea7587dce2712febcddabf2535b491338a8
marcozeller <mzeller@ethz.ch>
2016-09-08 15:13:35 +0200

    triangle class added

---
+++ Trigonometrysolver.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+
+class triangle
+{
+  private:
+    double a;
+    double b;
+    double c;
+    
+    double alpha;
+    double beta;
+    double gamma;
+    
+  public:
+    double get_a();
+    double get_b();
+    double get_c();
+    double get_gamma();
+    double get_beta();
+    double get_gamma();
+    
+    double set_a(a) this.a = a;
+    double set_b(b) this.b = b;
+    double set_c(c) this.c = c;
+    double set_alpha(alpha) this.alpha = alpha;
+    double set_beta(beta) this.beta = beta;
+    double set_gamma(gamma) this.gamma = gamma;
+    
+    void solve_rest
+
+}
+
+
+
+double main(){
+
+if(c==0 && beta==0 && gamma==0)
+cyz()
+
+if(b==0 && beta==0 && gamma==0)
+byz()
+
+if(b==0 && c==0 && gamma==0)
+bcz()
+
+if(b==0 && c==0 && beta==0)
+bcy()
+
+if(c==0 && alpha==0 && gamma==0)
+cxz()
+
+if(a==0 && alpha==0 && gamma==0)
+axz()
+
+if(a==0 && c==0 && beta==0)
+acy()
+
+if(a==0 && c==0 && gamma==0)
+acz()
+
+if(b==0 && alpha==0 && beta==0)
+bxy()
+
+if(a==0 && alpha==0 && beta==0)
+axy()
+
+if(a==0 && b==0 && beta==0)
+aby()
+
+if(a==0 && b==0 && gamma==0)
+abz()
+
+double ()
+
+
+
+       
+       }
