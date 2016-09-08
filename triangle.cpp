acdef02b222624e663792796170663e7d0eae887
marcozeller <mzeller@ethz.ch>
2016-09-08 16:35:08 +0200

    file structure clean

---
+++ triangle.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cmath>
+#include <trigonometrysolver.hh>
+
+class triangle
+{
+  private:
+    double a,
+            b,
+            c;
+    
+    double alpha,
+            beta,
+            gamma;
+    
+  public:
+    triangle():
+    double get_a(){
+      return this.a
+    };
+    double get_b(){
+      return this.b
+    };
+    double get_c(){
+      return this.c
+    };
+            
+    double get_gamma();
+    double get_beta();
+    double get_gamma();
+    
+    void set_a(a) this.a = a;
+    void set_b(b) this.b = b;
+    void set_c(c) this.c = c;
+    void set_alpha(alpha) this.alpha = alpha;
+    void set_beta(beta) this.beta = beta;
+    void set_gamma(gamma) this.gamma = gamma;
+    
+    void solve_rest
+
+}
+
+
+
+double main(){
+
+if(c==0 && beta==0 && gamma==0)
+cyz()
+
+if(b==0 && beta==0 && gamma==0)
+byz()
+
+if(b==0 && c==0 && gamma==0)
+bcz()
+
+if(b==0 && c==0 && beta==0)
+bcy()
+
+if(c==0 && alpha==0 && gamma==0)
+cxz()
+
+if(a==0 && alpha==0 && gamma==0)
+axz()
+
+if(a==0 && c==0 && beta==0)
+acy()
+
+if(a==0 && c==0 && gamma==0)
+acz()
+
+if(b==0 && alpha==0 && beta==0)
+bxy()
+
+if(a==0 && alpha==0 && beta==0)
+axy()
+
+if(a==0 && b==0 && beta==0)
+aby()
+
+if(a==0 && b==0 && gamma==0)
+abz()
+
+double ()
+
+
+
+       
+       }
