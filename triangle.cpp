01c84da9fc410e576817260e871b21d5ad10ab98
Marco Zeller <mzeller@stud.phys.ethz.ch>
2016-09-10 00:31:38 +0200

    new function for two sides one angle, implemented - wrong names alpha-gamma changed

--- triangle.cpp
+++ triangle.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 #include "triangle.hpp"
-
+double PI = 3.14159;
 
 Triangle::Triangle(){
     a = 0;
@@ -13,8 +13,58 @@ Triangle::Triangle(){
 	}
     	
 void Triangle::solve_rest(){
-	if(a != 0 && b != 0 && c != 0 )
-		alpha = std::acos((a*a + b*b - c*c) / (2*a*b));
+	
+  if(a != 0 && b != 0 && c != 0 ){
+
+    alpha = std::acos((b*b + c*c - a*a) / (2*b*c));
+		gamma = std::acos((a*a + b*b - c*c) / (2*a*b));
 		beta = std::acos((a*a + c*c - b*b) / (2*a*c));
-		gamma = std::acos((b*b + c*c - a*a) / (2*b*c));				
+  }
+  else if(a != 0 && b != 0){
+    if(alpha !=0){
+
+    }
+    if(beta !=0){
+
+    }
+      
+    if(gamma !=0){
+
+    }
+  }
+  else if(a != 0 && c != 0){
+    if(alpha !=0){
+      
+    }
+    if(beta !=0){
+
+    }
+      
+    if(gamma !=0){
+      
+    }  
+  }
+  else if(b != 0 && c != 0){
+    if(alpha !=0){
+      a = std::sqrt(b*b + c*c - 2*b*c*std::cos(alpha));
+    }
+    if(beta !=0){
+      a = std::cos(beta) + std::sqrt(std::cos(beta)*std::cos(beta)-c*c+b*b);
+    }
+      
+    if(gamma !=0){
+      
+    }
+  }
+  else if(alpha != 0 && beta != 0){
+    gamma = PI - alpha - beta;
+  }
+  else if(alpha != 0 && gamma != 0){
+    beta = PI - alpha - gamma;
+  }
+  else if(beta != 0 && gamma != 0){
+    alpha = PI - beta - gamma;
+  }
+
+
 }
--- trigonometrysolver.cpp
+++ trigonometrysolver.cpp
@@ -5,15 +5,19 @@ int main(){
 
 Triangle Mauro;
 
-Mauro.set_a(3.);
+//Mauro.set_a(3.);
 Mauro.set_b(4.);
 Mauro.set_c(5.);
+Mauro.set_alpha(0.643501);
+//Mauro.set_alpha(0.927295);
+//Mauro.set_alpha(1.5708);
+
 
 Mauro.solve_rest();
 
-std::cout << Mauro.get_a() << "°  und " << Mauro.get_b() << "°  und " << Mauro.get_c() << "° ";
+std::cout << "a="<< Mauro.get_a() << "cm und b=" << Mauro.get_b() << "cm und c=" << Mauro.get_c() << "cm " << std::endl;
 
-std::cout << Mauro.get_alpha() << "°  und " << Mauro.get_beta() << "°  und " << Mauro.get_gamma() << "° ";
+std::cout << "alpha="<< Mauro.get_alpha() << "rad  und beta=" << Mauro.get_beta() << "rad  und gamma=" << Mauro.get_gamma() << "rad ";
 
 int a = 0;
 std::cin >> a;
